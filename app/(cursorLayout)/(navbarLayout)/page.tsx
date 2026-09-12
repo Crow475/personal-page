027b0a3fdca36f57545097ad139bea2bb6c5b7b6
Crow475 <spicable475@gmail.com>
2026-09-12 16:39:25 +0400

    feat: add links to home page
    - add CTA link component

--- app/(cursorLayout)/(navbarLayout)/page.tsx
+++ app/(cursorLayout)/(navbarLayout)/page.tsx
@@ -4,15 +4,17 @@ import { inter, kanit } from "@/lib/fonts";
 
 import HomePageBackground from "@/components/elements/homePageBackground";
 
+import CTALink from "@/components/molecules/ctaLink";
+
 export default function Home() {
     return (
-        <main className="relative h-[200vh] w-full">
+        <main className="relative w-full">
             <HomePageBackground />
             <div className="h-20" role="presentation" />
             <article
-                className={`${inter.className} flex flex-col items-start justify-start space-y-4 px-10`}
+                className={`${inter.className} flex h-[calc(100svh-5rem)] flex-col items-start justify-start space-y-4 px-10`}
             >
-                <h1 className="fle flex-col items-start justify-start pb-5">
+                <h1 className="flex flex-col items-start justify-start pb-5">
                     <span
                         className={`${kanit.className} text-[200px] leading-none font-black text-white`}
                     >
@@ -26,6 +28,18 @@ export default function Home() {
                 <p className="text-xl text-neutral-400">
                     I do web development, programming and other stuff
                 </p>
+                <div className="flex flex-row items-center justify-start space-x-4 pt-10 pl-2">
+                    <CTALink
+                        href="/projects"
+                        text="My projects"
+                        variant="secondary"
+                    />
+                    <CTALink
+                        href="/#contact"
+                        text="Contact me"
+                        variant="primary"
+                    />
+                </div>
             </article>
         </main>
     );
