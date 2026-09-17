a73850728832d2c303185b40e1537465765c5c57
Crow475 <spicable475@gmail.com>
2026-09-17 15:19:37 +0400

    feat: add sanity CMS and studio

--- app/(cursorLayout)/layout.tsx
+++ app/(cursorLayout)/layout.tsx
@@ -7,7 +7,7 @@ import Cursor from "@/components/elements/cursor";
 export default function CursorLayout({ children }: LayoutProps<"/">) {
     return (
         <CursorProvider>
-            <div className="flex h-svh w-full scrollbar-thin scrollbar-thumb-white/40 scrollbar-track-transparent overflow-x-hidden overflow-y-scroll p-0.5">
+            <div className="flex h-svh w-full cursor-none scrollbar-thin scrollbar-thumb-white/40 scrollbar-track-transparent overflow-x-hidden overflow-y-scroll p-0.5 motion-reduce:cursor-default">
                 {children}
                 <Cursor />
             </div>
